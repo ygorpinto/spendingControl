df13b93a5eb2f21f65aca90e6e2614ecf373e5ae
ygorpinto <ygor.pinto@hotmail.com>
2021-08-07 12:43:49 -0300

    ok

--- src/screens/SignIn/styles.js
+++ src/screens/SignIn/styles.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components/native';
 
 const Container = styled.SafeAreaView`
+flex: 1;
+justify-content: center;
+align-items: center;
+background-color: lightpink;
 `
 
 export default Container;
