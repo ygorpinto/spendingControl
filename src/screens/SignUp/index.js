8baa8d5a5d04ecb570aa3c5b68dfa313c57bf386
ygorpinto <ygor.pinto@hotmail.com>
2021-08-15 11:41:20 -0300

    ok'

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -5,7 +5,26 @@ import { Text } from 'react-native';
 const SignUp = () => {
     return (
         <Container>
-            <Text>Tela de Cadastro</Text>
+            <View>
+                <Text>Hot Company</Text>
+            </View>
+            <View>
+                <Text>Cria sua conta:</Text>
+                <TextInput 
+                placeholder="Digite seu usuário"
+                keyboardType="default"/>
+                <TextInput
+                placeholder="Digite sua senha" 
+                keyboardType="default"
+                textContentType="password"/>
+                <Button
+                title="Entrar"
+                />
+                <Text>Já possui uma conta?</Text>
+                <Button
+                onPress={handleNewAccount}
+                title="Faça Login"/>
+            </View>
         </Container>
     )
 }
